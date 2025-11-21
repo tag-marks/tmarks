a79cb29026afca4a80f5d509b769899820874112
tag-marks <abcdefghijklmnopqrstuvwxyzputi@gmail.com>
2025-11-22 01:13:32 +0800

    0000bug

--- tmarks/src/routes/index.tsx
+++ tmarks/src/routes/index.tsx
@@ -4,28 +4,6 @@ import { AppShell } from '@/components/layout/AppShell'
 import { FullScreenAppShell } from '@/components/layout/FullScreenAppShell'
 import { PublicAppShell } from '@/components/layout/PublicAppShell'
 import { ProtectedRoute } from '@/components/auth/ProtectedRoute'
-<<<<<<< HEAD
-=======
-import { LoginPage } from '@/pages/auth/LoginPage'
-import { RegisterPage } from '@/pages/auth/RegisterPage'
-import { BookmarksPage } from '@/pages/bookmarks/BookmarksPage'
-import { TabGroupsPage } from '@/pages/tab-groups/TabGroupsPage'
-import { TabGroupDetailPage } from '@/pages/tab-groups/TabGroupDetailPage'
-import { TrashPage } from '@/pages/tab-groups/TrashPage'
-import { StatisticsPage } from '@/pages/tab-groups/StatisticsPage'
-import { TodoPage } from '@/pages/tab-groups/TodoPage'
-import { ApiKeysPage } from '@/pages/settings/ApiKeysPage'
-import { ShareSettingsPage } from '@/pages/settings/ShareSettingsPage'
-import { ImportExportPage } from '@/pages/settings/ImportExportPage'
-import { PermissionsPage } from '@/pages/settings/PermissionsPage'
-import { GeneralSettingsPage } from '@/pages/settings/GeneralSettingsPage'
-import { PublicSharePage } from '@/pages/share/PublicSharePage'
-import { ExtensionPage } from '@/pages/extension/ExtensionPage'
-import { AboutPage } from '@/pages/info/AboutPage'
-import { HelpPage } from '@/pages/info/HelpPage'
-import { PrivacyPage } from '@/pages/info/PrivacyPage'
-import { TermsPage } from '@/pages/info/TermsPage'
->>>>>>> a06a173a9a82a422a1873e134c9147633d967634
 
 // 懒加载页面组件
 const LoginPage = lazy(() => import('@/pages/auth/LoginPage').then(m => ({ default: m.LoginPage })))
@@ -75,7 +53,6 @@ export function AppRouter() {
           <Route path="/share/:slug" element={<PublicSharePage />} />
         </Route>
 
-<<<<<<< HEAD
         {/* 受保护的路由 */}
         <Route element={<ProtectedRoute />}>
           {/* 全屏布局 - 用于书签和标签页组 */}
@@ -102,25 +79,6 @@ export function AppRouter() {
             <Route path="/terms" element={<TermsPage />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Route>
-=======
-        {/* 常规布局 - 用于设置和其他页面 */}
-        <Route element={<AppShell />}>
-          <Route path="/tab/todo" element={<TodoPage />} />
-          <Route path="/tab/trash" element={<TrashPage />} />
-          <Route path="/tab/statistics" element={<StatisticsPage />} />
-          <Route path="/tab/:id" element={<TabGroupDetailPage />} />
-          <Route path="/settings/general" element={<GeneralSettingsPage />} />
-          <Route path="/api-keys" element={<ApiKeysPage />} />
-          <Route path="/share-settings" element={<ShareSettingsPage />} />
-          <Route path="/import-export" element={<ImportExportPage />} />
-          <Route path="/permissions" element={<PermissionsPage />} />
-          <Route path="/extension" element={<ExtensionPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/help" element={<HelpPage />} />
-          <Route path="/privacy" element={<PrivacyPage />} />
-          <Route path="/terms" element={<TermsPage />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
->>>>>>> a06a173a9a82a422a1873e134c9147633d967634
         </Route>
       </Routes>
     </Suspense>
